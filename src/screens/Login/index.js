be868b6f8ad56f63796cae53a285b1a71a0c53f0
MarquisAlexander <marquis.alexander@outlook.com>
2022-07-27 18:21:07 -0300

    first commit

---
+++ src/screens/Login/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import * as S from './styles';
+
+export function Login() {
+    return (
+        <S.Container>
+            <S.Title>ola mundo</S.Title>
+        </S.Container>
+    )
+}
